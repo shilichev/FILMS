dedf1520d38b4e0cf4cbfbdeaa5658f5b6161669
shilichev <52910393+shilichev@users.noreply.github.com>
2020-07-27 15:40:43 +0300

    first

---
+++ main.js
@@ -0,0 +1,88 @@
+let randomId = [];
+API_ID = [];
+
+const getRandomInt = () => {
+  for (i = 0; i < 5; i++) {
+    randomId.push(`${Math.floor(Math.random() * Math.floor(9999999))}`);
+  }
+  return randomId;
+};
+
+const showFilmsFromSearch = (films) => {
+  for (i = 0; i < 5; i++) {
+    console.log(films);   
+    $("ul").append(`<li><img src="${films.Search[i].Poster}"></li>`);
+  }
+};
+
+const getAPI = () => {
+  for (i = 0; i < 5; i++) {
+    API_ID.push(`http://www.omdbapi.com/?i=tt${randomId[i]}&apikey=d139291d`);
+  }
+  return API_ID;
+};
+console.log(API_ID);
+
+const getRandomFilms = () => {
+  $.ajax({
+    url: API_ID[0],
+    dataType: "json",
+    success: showFilmsFromSearch,
+  });
+};
+
+$(document).ready(function () {
+  getRandomInt();
+  getAPI();
+  showFilmsFromSearch();
+});
+//   console.log(films.Search[1].Poster);
+// };
+
+// console.log(randomId);
+// const getFilmsList = (filmsList) => {
+//   // filteredContributors = filmsList;
+//   // contributors = sorteringContributors(contributorsList, true);
+//   showFilmsFromSearch(filmsList);
+// };
+// const getFilms = () => {
+//   //   for (i = 0; i < 5; i++) {
+//   $.ajax({
+//     url: API_ID,
+//     dataType: "json",
+//     success: getFilmsList,
+//   });
+//   //   }
+// };
+// getFilms();
+
+// getFilmsList();
+// console.log(getFilmsList());
+
+// $(document).ready(function () {
+//   /* конфигурация */
+//   let width = 130; // ширина картинки
+//   let count = 3; // видимое количество изображений
+
+//   let list = carousel.querySelector("ul");
+
+//   let position = 0; // положение ленты прокрутки
+
+//   carousel.querySelector(".prev").onclick = function () {
+//     // сдвиг влево
+//     position += width * count;
+//     // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+//     position = Math.min(position, 0);
+//     list.style.marginLeft = position + "px";
+//   };
+
+//   carousel.querySelector(".next").onclick = function () {
+//     // сдвиг вправо
+//     position -= width * count;
+//     // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+
+//     position = Math.max(position, -width * (10 - count));
+//     list.style.marginLeft = position + "px";
+//   };
+// });
+// getFilms();
